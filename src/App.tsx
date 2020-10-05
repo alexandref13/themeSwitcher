6f6166016b6f779dd50c13c13007e5ec09fb6e3c
Alexandre Fernandes <alexfernandes199922@gmail.com>
2020-10-05 02:15:49 -0300

    add theme switcher button to light and dark

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ThemeProvider } from 'styled-components'
 
 import light from './components/styles/theme/light'
+import dark from './components/styles/theme/dark'
 
 import Header from './components/Header';
 import Global from './components/styles/Global';
 
 function App() {
+
+  const [ theme, setTheme ] = useState(light)
+
+  const toggleTheme = () => {
+    setTheme(theme.title === 'light' ? dark : light)
+  }
+
   return (
-    <ThemeProvider theme={light}>
+    <ThemeProvider theme={theme}>
       <Global/> 
-      <Header />
+      <Header toggleTheme= {toggleTheme} />
     </ThemeProvider>
   );
 }
---
+++ src/components/styles/theme/dark.ts
@@ -0,0 +1,13 @@
+export default {
+  title: 'dark',
+
+  // usar para paleta de cores o site coolors.co/app
+
+  colors: {
+    primary: '#333',
+    secundary: '#c62e65',
+
+    background: '#222',
+    text: '#fff'
+  },
+}
