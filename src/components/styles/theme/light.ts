34617c4621bd0e741956112b874b93f2af65846d
Alexandre Fernandes <alexfernandes199922@gmail.com>
2020-10-05 01:49:55 -0300

    add theme light and page header

---
+++ src/components/styles/theme/light.ts
@@ -0,0 +1,13 @@
+export default {
+  title: 'light',
+
+  // usar para paleta de cores o site coolors.co/app
+
+  colors: {
+    primary: '#c62e65',
+    secundary: '#d63af9',
+
+    background: '#f5f5f5',
+    text: '#333'
+  },
+}
